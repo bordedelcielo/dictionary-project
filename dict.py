e6892cc31f8341aa5f8b8f5276dc57d7082d35bd
bordedelcielo <christopher.thrutchley@gmail.com>
2022-04-27 00:23:11 -0500

    first commit

---
+++ dict.py
@@ -0,0 +1,9 @@
+# This function creates a dictionary from a list.
+def create_dict(input_list):
+    output_dict = {}
+    for i,j in enumerate(input_list):
+        temp_dict = {i:j}
+        output_dict.update(temp_dict)
+    return output_dict
+
+print(create_dict(['jimmy','jimi','hendrix']))
